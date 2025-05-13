724df9bcea7cd4efcc09af163eaf17aa0d45e83b
Levi Omagbemi <leviomagbemi15@gmail.com>
2025-05-13 22:44:57 +0100

    Week02 learning activity handling DOM events - Create and book to book list

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,11 +2,26 @@ const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
 
-const li = document.createElement('li');
-const deleteButton = document.createElement("button");
 
-li.textContent = input.value;
-deleteButton.textContent = "❌";
+button.addEventListener("click", function() {
+  if (input.value.trim() !== ""){
+    const li = document.createElement("li");
+    const deleteButton = document.createElement("button");
 
-li.append(deleteButton);
-list.append(li);
+    li.textContent = input.value;
+    deleteButton.textContent = "❌";
+
+    li.append(deleteButton);
+    list.append(li);
+
+    deleteButton.addEventListener("click", function() {
+      list.removeChild(li);
+      input.focus();
+    });
+
+    input.value = "";
+  }
+});
+
+input.focus();
+
